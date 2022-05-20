c4ebe759cacc6e20422d98cc5f0d2916f98fc39e
Mohamed Ali <61656810+mohamedal1362001@users.noreply.github.com>
2022-05-20 21:21:44 +0200

    DataSet

--- Camera.py
+++ Camera.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
+from tensorflow.python import keras
+from tensorflow.keras.applications.mobilenet_v2 import  preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import videostream, VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def detecting_Mask(frame , facenet ,masknet):
+    (h,w) =  frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame , 1.0 ,(224, 224),(104.0, 177.0, 123.0))
+
+    facenet.setInput(blob)
+    detection = facenet.forward()
+    print(detection.shape)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    faces = []
+    locs = []
+    preds = []
+    for i in range(0, detection.shape[2]):
+        confidence = detection[0 , 0 , i ,2]
+        if confidence > 0.5:
+            Box = detection[0,0,i,3:7] * np.array([w,h,w,h])
+            (startX , startY , endX , endY) = Box.astype("int")
+
+            (startX,startY) =(max(0,startX),max(0,startY) )
+            (endX , endY) = (max(w-1,endX),max(h-1,endY) )
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face , cv2.COLOR_BGRA2RGB)
+            face = cv2.resize(face , (224,224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            faces.append(face)
+            locs.append( (startX , startY , endX , endY) )
+
+    if(len(faces)) > 0:
+        faces = np.array(faces , dtype="float32")
+        preds = masknet.predict(faces , batch_size = 32)
+
+    return (locs,preds)
+
+prototxtpath = r"D:\FaceMask\face_detector\deploy.prototxt"
+weightspath = r"D:\FaceMask\face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtpath , weightspath)
+
+masknet = load_model("mask_detector.model")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Starting video stream")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+vs = VideoStream(src = 0).start()
+
+
+while True :
+
+    frame = vs.read()
+    frame = imutils.resize(frame , width=400)
+
+    (locs ,preds) = detecting_Mask(frame ,faceNet ,masknet)
+    for (box , pred) in zip(locs,preds):
+        ( startX , startY , endX , endY) = box
+        (Mask , without_Mask) = pred
+        label = "Mask" if Mask > without_Mask else "No Mask"
+        color = (0,255,0) if label =="Mask" else (0,0,255)
+        label = "{}: {:.2f}%".format(label,max(Mask,without_Mask)*100)
+
+        cv2.putText(frame,label,(startX ,startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX ,0.45 ,color , 2)
+        cv2.rectangle(frame,(startX , startY),(endX , endY),color,2)
+    cv2.imshow("Frame",frame)
+    key = cv2.waitKey(1)& 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
